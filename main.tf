a74355cca9c67ff34323320410ce62b8e9301f8f
Atanas Chuchulev <atanas.v4@gmail.com>
2021-07-19 11:28:55 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+
+//   default_tags {
+//     tags = {
+//       Environment = "Test"
+//       Name        = "Provider Tag"
+//     }
+//   }
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Owner = "example"
+    Environment = "Test"
+    Name        = "Provider Tag"
+  }
+}
+
+// output "vpc_resource_level_tags" {
+//   value = aws_vpc.example.tags
+// }
+
+// output "vpc_all_tags" {
+//   value = aws_vpc.example.tags_all
+// }
